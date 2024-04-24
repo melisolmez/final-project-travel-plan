3781547084898f5ddcd215e7feebb84cd3bc27ad
Melis Olmez <olmezmelis204@gmail.com>
2024-04-24 22:49:59 +0300

    updated user repository

--- src/main/java/dev/melis/travelplanapp/repository/UserRepository.java
+++ src/main/java/dev/melis/travelplanapp/repository/UserRepository.java
@@ -2,9 +2,10 @@ package dev.melis.travelplanapp.repository;
 
 import dev.melis.travelplanapp.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends MongoRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
