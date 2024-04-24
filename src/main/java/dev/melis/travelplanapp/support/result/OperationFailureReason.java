20c1eca5d463956fc32b058f0bd9bbc5219177f8
Melis Olmez <olmezmelis204@gmail.com>
2024-04-24 22:51:45 +0300

    added operation result and reason

---
+++ src/main/java/dev/melis/travelplanapp/support/result/OperationFailureReason.java
@@ -0,0 +1,7 @@
+package dev.melis.travelplanapp.support.result;
+
+public enum OperationFailureReason {
+    NOT_FOUND,
+    CONFLICT,
+    UNAUTHORIZED, ALREADY_REPORTED, PRECONDITION_FAILED
+}
---
+++ src/main/java/dev/melis/travelplanapp/support/result/OperationResult.java
@@ -0,0 +1,6 @@
+package dev.melis.travelplanapp.support.result;
+
+public enum OperationResult {
+  SUCCESS,
+    FAILED
+}
