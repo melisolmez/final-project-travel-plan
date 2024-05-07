8287ef65f2b19b1bc3b581686f538526d64052a8
Melis Olmez <olmezmelis204@gmail.com>
2024-05-08 00:14:27 +0300

    updated

--- src/main/java/dev/melis/travelplanapp/service/jwt/JwtService.java
+++ src/main/java/dev/melis/travelplanapp/service/jwt/JwtService.java
@@ -7,12 +7,13 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+@Service
 public class JwtService {
     @Value("${jwt-key}")
     private String SECRET;
