2422629b7564f33223de14bed1c9f40e175b2554
Melis Olmez <olmezmelis204@gmail.com>
2024-06-03 23:47:34 +0300

    update

--- src/main/java/dev/melis/travelplanapp/model/User.java
+++ src/main/java/dev/melis/travelplanapp/model/User.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.annotation.processing.Generated;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
