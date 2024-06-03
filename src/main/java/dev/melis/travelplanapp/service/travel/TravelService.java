63d0a208f0a8a3c2d4dac0481e1e910ea039d3e1
Melis Olmez <olmezmelis204@gmail.com>
2024-06-03 23:49:41 +0300

    added travel service

---
+++ src/main/java/dev/melis/travelplanapp/service/travel/TravelService.java
@@ -0,0 +1,21 @@
+package dev.melis.travelplanapp.service.travel;
+
+import dev.melis.travelplanapp.model.Travel;
+import dev.melis.travelplanapp.support.result.CrudResult;
+import dev.melis.travelplanapp.support.result.UpdateResult;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Validated
+public interface TravelService {
+
+    CrudResult addTravelPlan(TravelServiceRequest request);
+    List<Travel> listAllTravelPlans(String userId);
+    Travel getTravelPlan(String travelId);
+
+    UpdateResult updateTravelInformation(String travelId, TravelServiceRequest request);
+    void deleteTravelPlan(String travelId);
+
+
+}
