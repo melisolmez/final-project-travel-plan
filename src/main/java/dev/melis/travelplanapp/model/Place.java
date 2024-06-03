21e7b9410b039c6eb2e8ab430e317fdb9841d25a
Melis Olmez <olmezmelis204@gmail.com>
2024-06-03 23:48:37 +0300

    added place model class

---
+++ src/main/java/dev/melis/travelplanapp/model/Place.java
@@ -0,0 +1,32 @@
+package dev.melis.travelplanapp.model;
+
+public class Place {
+
+    private String isim;
+    private String tanitim;
+    private String resim;
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getTanitim() {
+        return tanitim;
+    }
+
+    public void setTanitim(String tanitim) {
+        this.tanitim = tanitim;
+    }
+
+    public String getResim() {
+        return resim;
+    }
+
+    public void setResim(String resim) {
+        this.resim = resim;
+    }
+}
